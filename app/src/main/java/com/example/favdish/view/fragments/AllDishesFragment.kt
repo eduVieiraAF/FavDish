53656095868a1eb9fba7a5e19c03cf8c79dcfaf0
eduVieiraAF <edu_vieira@hotmail.com>
2022-07-04 10:41:41 -0300

    MVVM and icon
    Rearranged the packages and added the 'add' button to home fragment, which now is called all_dishes

---
+++ app/src/main/java/com/example/favdish/view/fragments/AllDishesFragment.kt
@@ -0,0 +1,67 @@
+package com.example.favdish.view.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.favdish.R
+import com.example.favdish.databinding.FragmentAllDishesBinding
+import com.example.favdish.view.activities.AddUpdateActivity
+import com.example.favdish.viewmodel.HomeViewModel
+
+@Suppress("DEPRECATION")
+class AllDishesFragment : Fragment() {
+
+    private var _binding: FragmentAllDishesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentAllDishesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_dishes, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_add_dish  -> {
+                startActivity(Intent(requireActivity(), AddUpdateActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
