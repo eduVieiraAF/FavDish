ac8512b7cfb428b880353ede56a971341908b91b
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-28 10:41:11 -0300

    Popup menu UI:
    → Edit dish;
    → Delete dish:

--- app/src/main/java/com/example/favdish/view/adapters/FavDishAdapter.kt
+++ app/src/main/java/com/example/favdish/view/adapters/FavDishAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.favdish.view.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.favdish.R
 import com.example.favdish.databinding.ItemDishLayoutBinding
 import com.example.favdish.model.entities.FavDish
 import com.example.favdish.view.fragments.AllDishesFragment
@@ -19,12 +23,14 @@ class FavDishAdapter(private val fragment: Fragment) : RecyclerView
 
         val ivDishImage = view.ivDishImage
         val tvTitle = view.tvDishTitle
+        val ibMore = view.ibMore
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         val binding: ItemDishLayoutBinding = ItemDishLayoutBinding.inflate(
-            LayoutInflater.from(fragment.context), parent, false)
+            LayoutInflater.from(fragment.context), parent, false
+        )
 
         return ViewHolder(binding)
     }
@@ -46,6 +52,26 @@ class FavDishAdapter(private val fragment: Fragment) : RecyclerView
                 fragment.dishDetails(dish)
             }
         }
+
+        holder.ibMore.setOnClickListener {
+            val popup = PopupMenu(fragment.context, holder.ibMore)
+
+            popup.menuInflater.inflate(R.menu.menu_adapter, popup.menu)
+            popup.setOnMenuItemClickListener {
+                if (it.itemId == R.id.action_edit_dish) {
+                    Log.i("Clicked", "Edit")
+                } else if (it.itemId == R.id.action_delete_dish){
+                    Log.i("Clicked", "Delete")
+                }
+
+                true
+            }
+
+            popup.show()
+        }
+
+        if (fragment is AllDishesFragment) holder.ibMore.visibility = View.VISIBLE
+        else if (fragment is FavoriteDishesFragment) holder.ibMore.visibility = View.GONE
     }
 
     override fun getItemCount(): Int {
