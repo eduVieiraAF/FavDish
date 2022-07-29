7d47cbd4edd4068dec029b2f08075dc3937b8b3b
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-29 16:34:24 -0300

    Random dish setup pt.1:
    → preparubg API calls.

--- app/src/main/java/com/example/favdish/utils/Constants.kt
+++ app/src/main/java/com/example/favdish/utils/Constants.kt
@@ -11,6 +11,12 @@ object Constants {
     const val ALL_ITEMS: String = "All"
     const val FILTER_SELECTION: String = "FilterSelection"
 
+    const val API_ENDPOINT: String = "recipes/random"
+    const val API_KEY: String = "apiKey"
+    const val LIMIT_LICENSE: String = "limitLicense"
+    const val TAGS: String = "tags"
+    const val NUMBER: String = "number"
+
     fun dishTypes(): ArrayList<String> {
         val list = ArrayList<String>()
 
