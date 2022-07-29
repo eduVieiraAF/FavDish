4c9de9a170170dd0264ae26d21eed9424643d45f
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-29 15:07:36 -0300

    Random dish setup pt.1:
    → Implementation: Retrofit;
    → RandomDishFragment;
    → fragment_dish_details.xml.

--- app/src/main/java/com/example/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/RandomDishFragment.kt
@@ -4,40 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.favdish.databinding.FragmentRandomDishBinding
-import com.example.favdish.viewmodel.RandomDishViewModel
 
 class RandomDishFragment : Fragment() {
-
-    private var _binding: FragmentRandomDishBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private var mBinding: FragmentRandomDishBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this)[RandomDishViewModel::class.java]
+        mBinding = FragmentRandomDishBinding.inflate(inflater, container, false)
 
-        _binding = FragmentRandomDishBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return mBinding!!.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mBinding = null
     }
 }
