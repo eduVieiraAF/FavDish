834338e79a3a865225c58cc5d2c42a9e877227e3
Eduardo Vieira <edu_vieira@hotmail.com>
2022-08-01 14:35:45 -0300

    Random dish setup pt5:
    → Add random dish to favorites and DB.

--- app/src/main/java/com/example/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/RandomDishFragment.kt
@@ -7,13 +7,21 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.favdish.R
+import com.example.favdish.application.FavDishApplication
 import com.example.favdish.databinding.FragmentRandomDishBinding
+import com.example.favdish.model.entities.FavDish
 import com.example.favdish.model.entities.RandomDish
+import com.example.favdish.utils.Constants
 import com.example.favdish.utils.capitalized
+import com.example.favdish.viewmodel.FavDishViewModel
+import com.example.favdish.viewmodel.FavDishViewModelFactory
 import com.example.favdish.viewmodel.RandomDishViewModel
 
 class RandomDishFragment : Fragment() {
@@ -76,17 +84,20 @@ class RandomDishFragment : Fragment() {
 
         var dishType = "Other"
 
-        if  (recipe.dishTypes.isNotEmpty()) {
-            dishType = recipe.dishTypes[0]
+        if (recipe.dishTypes.isNotEmpty()) {
+            dishType = recipe.dishTypes[0].capitalized()
             mBinding!!.tvType.text = dishType.capitalized()
         }
 
-        mBinding!!.tvCategory.text = "Other"
+        mBinding!!.tvCategory.text = dishType.capitalized()
 
         var ingredients = ""
         for (value in recipe.extendedIngredients) {
-            if (ingredients.isEmpty()) ingredients = value.original
-            else ingredients = "ingredients \n ${value.original}"
+            if (ingredients.isEmpty()) {
+                ingredients = value.original
+            } else {
+                ingredients = ingredients + ", \n" + value.original
+            }
         }
 
         mBinding!!.tvIngredients.text = ingredients
@@ -105,6 +116,42 @@ class RandomDishFragment : Fragment() {
             R.string.lbl_estimate_cooking_time,
             recipe.readyInMinutes.toString()
         )
+
+        mBinding!!.ivFavoriteDish.setOnClickListener {
+            val randomDishDetails = FavDish(
+                recipe.image,
+                Constants.DISH_IMAGE_SOURCE_ONLINE,
+                recipe.title.capitalized(),
+                dishType.capitalized(),
+                "Other",
+                ingredients,
+                recipe.readyInMinutes.toString(),
+                recipe.instructions,
+                true
+            )
+
+            val mFavDishViewModel: FavDishViewModel by viewModels {
+                FavDishViewModelFactory(
+                    (requireActivity()
+                        .application as FavDishApplication).repository
+                )
+            }
+
+            mFavDishViewModel.insert(randomDishDetails)
+
+            mBinding!!.ivFavoriteDish.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireActivity(),
+                    R.drawable.ic_favorite_selected
+                )
+            )
+
+            Toast.makeText(
+                requireActivity(),
+                resources.getString(R.string.msg_added_to_favorites),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onDestroy() {
